3eaff9412c25d8a50f8459d7a8ed8c7e8a0152f7
qq476743842 <476743842@qq.com>
2019-01-28 11:43:10 +0800

    配置文件

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const fs = require('fs')
 const path = require('path')
 const pkg = require('./package.json')
 const file = require('./scripts/file')
+const login = require('./scripts/login')
+/**查看cli版本 */
 program
-  .version(pkg.version)
+  .version(pkg.version);
+/**登录操作 */
+program
+  .command('login')
+  .description('登录至软捷后台,只需登录一次,以后将会自动登录!')
+  .action(() => {
+    login.beforeLogin();
+  });
 
-program.parse(process.argv)
-module.exports={file}
+program.parse(process.argv);
+module.exports={file};
 
