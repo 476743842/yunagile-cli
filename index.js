dfb093f652ab14f381052aeabdf9424db5746746
qq476743842 <476743842@qq.com>
2019-01-28 15:18:07 +0800

    首页已经配置文件

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path')
 const pkg = require('./package.json')
 const file = require('./scripts/file')
 const login = require('./scripts/login')
+/**导入通知的命令 */
+const notice = require('./app/notice/cmd')
 /**查看cli版本 */
 program
   .version(pkg.version);
@@ -13,8 +15,24 @@ program
   .command('login')
   .description('登录至软捷后台,只需登录一次,以后将会自动登录!')
   .action(() => {
-    login.beforeLogin();
+    login.beforeLogin(true);
   });
+/**强制重新登录 */
+program
+  .command('relogin')
+  .description('强制重新登录!')
+  .action(() => {
+    login.beforeLogin(false);
+  });
+/**退出登录 */
+program
+  .command('logout')
+  .description('退出登录!(还没完善)')
+  .action(() => {
+    console.log("退出登录成功");
+  });
+
+notice.registerCmd(program);
 
 program.parse(process.argv);
 module.exports={file};
